0daf3d1d3c1984606cf8cb5b8812f24a9cc395eb
Thiago Scodeler <tscodeler@gmail.com>
2015-10-02 22:21:05 -0300

    User Counter

--- SmartWaterApp/api/report/index.php
+++ SmartWaterApp/api/report/index.php
@@ -24,33 +24,39 @@ $app->get('/totalByUser/:cpf', function ($cpf)  {
 });
 
 $app->get('/monthTotalByUser/:cpf', function ($cpf)  {
-  $sql = "SELECT max(water_flow) as total from history h join boards b
-  on b.mac_address = h.mac_address where b.cpf_user=:cpf";
   try {
     $conn = new Connection();
     $db = $conn->getConnection();
+
+    $sql = "SELECT max(water_flow) as total from history h join boards b
+    on b.mac_address = h.mac_address where b.cpf_user=:cpf";
     $stmt = $db->prepare($sql);
     $stmt->bindParam("cpf", $cpf);
     $stmt->execute();
     $monthTotal = $stmt->fetchObject();
+
     $sql = "SELECT max(water_flow) as total from history h join boards b on
     b.mac_address = h.mac_address where b.cpf_user=:cpf
     and extract(MONTH FROM time_register) =:month and extract(YEAR FROM time_register) =:year";
-    $stmt = $db->prepare($sql);
+
     $month = date('m',strtotime('-1 month'));
     if($month == 12) {
       $year = date('Y',strtotime('-1 year'));
     } else {
       $year = date('Y');
     }
+
+    $stmt = $db->prepare($sql);
     $stmt->bindParam("cpf",$cpf);
     $stmt->bindParam("month",$month);
     $stmt->bindParam("year",$year);
     $stmt->execute();
     $beforeMonthTotal = $stmt->fetchObject();
     $db = null;
-    $total['total'] = $monthTotal['total']-$beforeMonthTotal['total'];
+
+    $total['total'] = ($monthTotal->total) - ($beforeMonthTotal->total);
     echo json_encode($total);
+
   } catch(PDOException $e) {
     echo '{"error":{"text":'. $e->getMessage() .'}}';
   }
--- SmartWaterApp/user/app/controllers/dashboardController.js
+++ SmartWaterApp/user/app/controllers/dashboardController.js
@@ -1,6 +1,44 @@
-app.controller('DashboardCtrl', function($rootScope, $location,$cookies)
+app.controller('DashboardCtrl', function($http, $rootScope, $location,$cookies)
 {
    $rootScope.activetab = $location.path();
      $rootScope.pageTitle = 'Welcome';
      userCPF = $cookies.get('userCPF');
+
+     //set counters
+     setGeneralTotal($http,userCPF);
+     setMonthTotal($http,userCPF);
 });
+
+function setGeneralTotal($http, userCPF){
+  GeneralCounter = new FlipClock($('.generalTotal'), 1, {
+    clockFace: 'Counter'
+  });
+  $http.get('../api/report/totalByUser/'+userCPF).success(function(data) {
+    GeneralCounter.setValue(data.total);
+  });
+
+  setTimeout(function() {
+		setInterval(function() {
+      $http.get('../api/report/totalByUser/'+userCPF).success(function(data) {
+        GeneralCounter.setValue(data.total);
+      });
+		}, 10000);
+	});
+}
+
+function setMonthTotal($http, userCPF){
+  monthCounter = new FlipClock($('.monthTotal'), 1, {
+    clockFace: 'Counter'
+  });
+  $http.get('../api/report/monthTotalByUser/'+userCPF).success(function(data) {
+    monthCounter.setValue(data.total);
+  });
+
+  setTimeout(function() {
+		setInterval(function() {
+      $http.get('../api/report/monthTotalByUser/'+userCPF).success(function(data) {
+        monthCounter.setValue(data.total);
+      });
+		}, 10000);
+	});
+}
